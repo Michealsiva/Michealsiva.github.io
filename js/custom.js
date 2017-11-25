1847690bddf087738c5288b9a54382b1a839fc95
M. Sivakumar <33742240+Michealsiva@users.noreply.github.com>
2017-11-25 17:44:28 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -12,17 +12,7 @@
         $('#collapseEx').collapse('hide');
     });
     
-    $('#portfolio a').not('.btn').each(function() {
-     var a = new RegExp('/' + window.location.host + '/');
-     if(!a.test(this.href)) {
-         $(this).click(function(event) {
-             event.preventDefault();
-             event.stopPropagation();
-             window.open(this.href, '_blank');
-         });
-     }
-  });
-
+    
     /* WOW.js init */
     new WOW().init();
     var submitted = false;
