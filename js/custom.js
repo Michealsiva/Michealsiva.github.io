f05be1cd0e0a61f7b3df55585e2df834b916cd29
M. Sivakumar <33742240+Michealsiva@users.noreply.github.com>
2017-11-25 18:26:37 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -21,16 +21,7 @@
         $('#gform').prepend('Your submission has been processed...');
     });
 
-    $('a').not(".btn").each(function() {
-        var a = new RegExp('/' + window.location.host + '/');
-        if (!a.test(this.href)) {
-            $(this).click(function(event) {
-                event.preventDefault();
-                event.stopPropagation();
-                window.open(this.href, '_blank');
-            });
-        }
-    });
+   
 
     $('.ddd').ripples({
         resolution: 512,
